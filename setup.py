571fe2e6df1dbca09d8d662dd08a10db827422e6
Marc Brinkmann <git@marcbrinkmann.de>
2014-03-19 02:00:30 +0100

    Shipped necessary templates with package.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     description='Shows reflection/configuration to aid the development of '
                 'Flask applications.',
     long_description=read('README.rst'),
+    include_package_data=True,
     author='Marc Brinkmann',
     author_email='git@marcbrinkmann.de',
     url='http://github.com/mbr/flask-debug',
